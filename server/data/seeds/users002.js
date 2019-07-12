a1d8720c9f33338f0c2300c00b4b8b7fffb30256
JustinAndrade <j.andrade.john@gmail.com>
2019-07-11 21:26:09 -0400

    users table created

---
+++ server/data/seeds/users002.js
@@ -0,0 +1,7 @@
+exports.seed = function(knex, Promise) {
+	// Deletes ALL existing entries
+	return knex('users').del().then(function() {
+		// Inserts seed entries
+		return knex('users').insert([ { id: 1, username: 'lambda', password: 'password' } ]);
+	});
+};
