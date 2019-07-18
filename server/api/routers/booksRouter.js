37cfc809c395d9c843457f6bc98cf1a7dd25e73a
JustinAndrade <j.andrade.john@gmail.com>
2019-07-18 01:20:23 -0400

    goodreads endpoint added

--- server/api/routers/booksRouter.js
+++ server/api/routers/booksRouter.js
@@ -2,15 +2,32 @@ const { getBooks, findBookReviews } = require('../helpers');
 const router = require('express').Router();
 const db = require('../../data/dbConfig.js');
 
-import request from 'request-promise';
-import { parseString } from 'xml2js';
+const request = require('request-promise');
+const { parseString } = require('xml2js');
+
+// goodreads search
+router.get('/search', (req, res) => {
+	request
+		.get(`https://www.goodreads.com/search/index.xml?key=QWRcP15MmSKQuoMRbViLg&q=${req.query.q}`)
+		.then((result) =>
+			parseString(result, (err, goodreadsResult) =>
+				res.json({
+					books: goodreadsResult.GoodreadsResponse.search[0].results[0].work.map((work) => ({
+						goodreadsId: work.best_book[0].id[0]._,
+						title: work.best_book[0].title[0],
+						author: work.best_book[0].author[0].name[0],
+						book_img: [ work.best_book[0].image_url[0] ]
+					}))
+				})
+			)
+		);
+});
 
 router.get('/', async (req, res) => {
 	try {
 		const books = await getBooks();
 		if (books) {
 			res.status(200).json(books);
-			console.log(books);
 		} else {
 			res.status(404).json({ message: 'Books could not be found' });
 		}
@@ -37,24 +54,6 @@ router.get('/:id', async (req, res) => {
 	} catch (err) {
 		res.status(500).json({ message: err });
 	}
-});
-
-// goodreads search
-router.get('/search', (req, res) => {
-	request
-		.get(`https://www.goodreads.com/search/index.xml?key=QWRcP15MmSKQuoMRbViLg&q=${req.query.q}`)
-		.then((result) =>
-			parseString(result, (err, goodReadsResult) =>
-				res.json({
-					books: goodReadsResult.GoodreadsResponse.search[0].result[0].work.map((work) => ({
-						goodreadsId: work.best_book[0].id[0]._,
-						title: work.best_book[0].title[0],
-						author: work.best_book[0].author[0].name[0],
-						book_img: [ work.best_book[0].image_url[0] ]
-					}))
-				})
-			)
-		);
 });
 
 module.exports = router;
