9de58884158b460a5257f0905903b6a9391284ea
JustinAndrade <j.andrade.john@gmail.com>
2019-07-17 22:48:30 -0400

    goodreads search implemented

---
+++ server/api/goodreads/goodreads.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+	try {
+		const books = await getBooks();
+		if (books) {
+			res.status(200).json(books);
+			console.log(books);
+		} else {
+			res.status(404).json({ message: 'Books could not be found' });
+		}
+	} catch (err) {
+		res.status(500).json({ message: err });
+	}
+});
--- server/api/routers/booksRouter.js
+++ server/api/routers/booksRouter.js
@@ -2,6 +2,9 @@ const { getBooks, findBookReviews } = require('../helpers');
 const router = require('express').Router();
 const db = require('../../data/dbConfig.js');
 
+import request from 'request-promise';
+import { parseString } from 'xml2js';
+
 router.get('/', async (req, res) => {
 	try {
 		const books = await getBooks();
@@ -34,6 +37,24 @@ router.get('/:id', async (req, res) => {
 	} catch (err) {
 		res.status(500).json({ message: err });
 	}
+});
+
+// goodreads search
+router.get('/search', (req, res) => {
+	request
+		.get(`https://www.goodreads.com/search/index.xml?key=QWRcP15MmSKQuoMRbViLg&q=${req.query.q}`)
+		.then((result) =>
+			parseString(result, (err, goodReadsResult) =>
+				res.json({
+					books: goodReadsResult.GoodreadsResponse.search[0].result[0].work.map((work) => ({
+						goodreadsId: work.best_book[0].id[0]._,
+						title: work.best_book[0].title[0],
+						author: work.best_book[0].author[0].name[0],
+						book_img: [ work.best_book[0].image_url[0] ]
+					}))
+				})
+			)
+		);
 });
 
 module.exports = router;
